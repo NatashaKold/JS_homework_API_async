c6e3de2e0450e3a22f27df2dea12fc3d2a0bfb9e
NatashaKold <n.koldunova@gmail.com>
2025-03-18 14:56:49 +0300

    change small

--- addpost.js
+++ addpost.js
@@ -17,9 +17,9 @@ function addPost(){
 
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
-        body: JSON.stringify({
+        body: JSON.stringify(
             newPost
-        }),
+        ),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
